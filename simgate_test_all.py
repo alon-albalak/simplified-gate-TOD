2d10df118616487616a48b22c054a9eaefedd218
Alon Albalak <alonalbalak@gmail.com>
2020-12-17 08:29:03 -0800

    let model to test be a parameter

--- simgate_test_all.py
+++ simgate_test_all.py
@@ -4,9 +4,7 @@ from models.simgate_TRADE import TRADE
 from utils.simgate_multiwoz import prepare_data
 
 kwargs = utils.utils.parse_args()
-kwargs['binary_gates'] = True
-kwargs['USR_SYS_tokens'] = True
-model_dir = "save/binarySGate_USRSYStokens_bs120-TRADE-multiwozDST"
+model_dir = "save/{}-TRADE-multiwozDST".format(kwargs['experiment_ID'])
 logger = simple_logger(kwargs) if kwargs['log_path'] else None
 
 _, _, test, lang, slot_list, domain_gate, slot_gate, value_gate, domain_map, domain_slot_map, vocab_size_train = prepare_data(training=False, **kwargs)
